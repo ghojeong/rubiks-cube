1f765f46e8060befe784d2e67d59367d5f8579f1
고정완 (Can) <jw.gho@ecubelabs.com>
2020-12-09 01:28:21 +0900

    [feat] 3단계 dto 작성

---
+++ src/main/java/step3/dto/InputDto.java
@@ -0,0 +1,13 @@
+package step3.dto;
+
+public class InputDto {
+    private final String input;
+
+    public InputDto(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return input;
+    }
+}
